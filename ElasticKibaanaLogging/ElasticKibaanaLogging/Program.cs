46372eca48e3f3735dfda66033007346d39b6cce
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-03-29 10:43:17 +0530

    added code

--- ElasticKibaanaLogging/ElasticKibaanaLogging/Program.cs
+++ ElasticKibaanaLogging/ElasticKibaanaLogging/Program.cs
@@ -1,3 +1,7 @@
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
+
 namespace ElasticKibaanaLogging
 {
     public class Program
@@ -6,7 +10,18 @@ namespace ElasticKibaanaLogging
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
             // Add services to the container.
+            Log.Logger = new LoggerConfiguration().Enrich.FromLogContext().Enrich.WithMachineName()
+                .WriteTo.Debug().WriteTo.Console().WriteTo.Elasticsearch(
+                new ElasticsearchSinkOptions(new Uri(builder.Configuration["ElasticConfiguration:Uri"]))
+                {
+                    AutoRegisterTemplate = true,
+                    IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".","-")}-{DateTime.UtcNow:yyyy-MM} "
+                })
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(builder.Configuration)
+                .CreateLogger();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
